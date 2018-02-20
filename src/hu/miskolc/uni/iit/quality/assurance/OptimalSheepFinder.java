bdc0d3cc3c73761ee135ff8b340487150ea6f2ff
iasatan <satanadam@outlook.com>
2018-02-20 20:56:33 +0100

    jacket commit

--- src/hu/miskolc/uni/iit/quality/assurance/OptimalSheepFinder.java
+++ src/hu/miskolc/uni/iit/quality/assurance/OptimalSheepFinder.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import hu.miskolc.uni.iit.quality.assurance.model.Coordinate;
 import javafx.print.Collation;
@@ -11,17 +12,39 @@ import javafx.print.Collation;
 public class OptimalSheepFinder {
 
 	public static void main(String[] args) {
-		Collection<Coordinate> coordinates = readCoordinatesFromFile("coordinates.txt");
-		for (Coordinate coordinate : coordinates) {
-			System.out.println(coordinate.toString());
+		List<Coordinate> coordinates = readCoordinatesFromFile("coordinates.txt");
+		List<Coordinate> polygon = new ArrayList<>();
+		//No need to coontinue if no coordinates found
+		if(coordinates.isEmpty()) {
+			System.out.println("no sheep found");
+			return;
+		}
+		//If there are less than 3 points, doesn't matter which we choose
+		if(coordinates.size()<3) {
+			System.out.println(coordinates.get(0));
+			return;
+		}
+		//initial polygon
+		for (int i = 0; i <= 3; i++) {
+			polygon.add(coordinates.get(i));
+		}
+		for (int i = 4; i < coordinates.size(); i++) {
+			if(!coordinates.get(i).contains(polygon)/*insidePolygon(polygon)*/) {
+				System.out.println(coordinates.get(i));
+				polygon.add(coordinates.get(i));
+			}
+		}
+		System.out.println("asd");
+		for (Coordinate coordinate : polygon) {
+			System.out.println(coordinate);
 		}
 	}
 	
-	private static Collection<Coordinate> readCoordinatesFromFile(String filename){
-		Collection<Coordinate> coordinates = new ArrayList<>();
+	private static List<Coordinate> readCoordinatesFromFile(String filename){
+		List<Coordinate> coordinates = new ArrayList<>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filename));
-			Long count = Long.parseLong(br.readLine());
+			Integer count = Integer.parseInt(br.readLine());
 			String[] temp;
 			for (int i = 1; i <= count; i++) {
 				temp =br.readLine().split(" ");
@@ -32,5 +55,13 @@ public class OptimalSheepFinder {
 		}
 		return coordinates;
 	}
-
+	/*TODO: algoritmus: megkeresni a külső pontokat, a többit ki lehet dobni
+	 * első három pont alkot egy háromszöget
+	 * következő pont ha benne van a háromszögben törlődik,
+	 * ha nincs akkor új pontként felvesszük és négyszögre vizsgáljuk a következőt stb
+	 * 
+	 * Compute the oriented sum of angles between the point p and each of the polygon apices. If the total oriented angle is 360 degrees, the point is inside. If the total is 0, the point is outside.
+	 * I like this method better because it is more robust and less dependent on numerical precision.
+	 * Methods that compute evenness of number of intersections are limited because you can 'hit' an apex during the computation of the number of intersections.
+	*/
 }
--- src/hu/miskolc/uni/iit/quality/assurance/model/Coordinate.java
+++ src/hu/miskolc/uni/iit/quality/assurance/model/Coordinate.java
@@ -1,5 +1,8 @@
 package hu.miskolc.uni.iit.quality.assurance.model;
 
+import java.util.Collection;
+import java.util.List;
+
 public class Coordinate {
 	private int id;
 	private int x;
@@ -35,10 +38,39 @@ public class Coordinate {
 		this.y = y;
 	}
 
+	public boolean insidePolygon(Collection<Coordinate> polygon) {
+		double angle = 0.0;
+		for (Coordinate coordinate : polygon) {
+			angle += Math.atan2(y - coordinate.y, x - coordinate.x) * 180 / Math.PI;
+		}
+		System.out.println(angle);
+		if (angle <= 361 && angle >= 359)
+			return true;
+		return false;
+	}
+
+	public boolean contains(List<Coordinate> polygon) {
+		int crossings = 0;
+		for (int i = 0; i < polygon.size(); i++) {
+			int j = i + 1;
+			boolean cond1 = (polygon.get(i).y <= y) && (y < polygon.get(j).y);
+			boolean cond2 = (polygon.get(j).y <= y) && (y < polygon.get(i).y);
+			if (cond1 || cond2) {
+				// need to cast to double
+				if (x < (polygon.get(j).x - polygon.get(i).x) * (y - polygon.get(i).y)
+						/ (polygon.get(j).y - polygon.get(i).y) + polygon.get(i).x)
+					crossings++;
+			}
+		}
+		if (crossings % 2 == 1)
+			return true;
+		else
+			return false;
+	}
+
 	@Override
 	public String toString() {
 		return "Coordinate [id=" + id + ", x=" + x + ", y=" + y + "]";
 	}
-	
-	
+
 }
